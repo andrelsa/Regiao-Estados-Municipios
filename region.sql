6ef4b720075bedbdd1f4a4e5d5159324f916d21d
Andre <andre@labtime.ufg.br>
2019-07-01 09:16:35 -0300

    Paises, Regioes, Estados e Municipios

---
+++ region.sql
@@ -0,0 +1,7 @@
+-- Insert Data --
+
+Insert into tb_region (co_region, no_name) values (1, 'Norte');
+Insert into tb_region (co_region, no_name) values (2, 'Nordeste');
+Insert into tb_region (co_region, no_name) values (3, 'Sudeste');
+Insert into tb_region (co_region, no_name) values (4, 'Sul');
+Insert into tb_region (co_region, no_name) values (5, 'Centro-Oeste');
---
+++ state.sql
@@ -0,0 +1,29 @@
+-- Insert Data --
+
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (1, 'Acre', 'AC', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (2, 'Alagoas', 'AL', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (3, 'Amapá', 'AP', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (4, 'Amazonas', 'AM', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (5, 'Bahia', 'BA', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (6, 'Ceará', 'CE', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (7, 'Distrito Federal', 'DF', 5);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (8, 'Espírito Santo', 'ES', 3);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (9, 'Goiás', 'GO', 5);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (10, 'Maranhão', 'MA', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (11, 'Mato Grosso', 'MT', 5);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (12, 'Mato Grosso do Sul', 'MS', 5);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (13, 'Minas Gerais', 'MG', 3);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (14, 'Pará', 'PA', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (15, 'Paraíba', 'PB', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (16, 'Paraná', 'PR', 4);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (17, 'Pernambuco', 'PE', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (18, 'Piauí', 'PI', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (19, 'Rio de Janeiro', 'RJ', 3);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (20, 'Rio Grande do Norte', 'RN', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (21, 'Rio Grande do Sul', 'RS', 4);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (22, 'Rondônia', 'RO', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (23, 'Roraima', 'RR', 1);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (24, 'Santa Catarina', 'SC', 4);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (25, 'São Paulo', 'SP', 3);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (26, 'Sergipe', 'SE', 2);
+Insert into tb_state (co_state, no_name, sg_uf, fk_state_region) values (27, 'Tocantins', 'TO', 1);
